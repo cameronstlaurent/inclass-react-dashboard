2c18ef1f6ff4cc85eab5da336ec5856aa3d1e460
cameronstlaurent <77070934+cameronstlaurent@users.noreply.github.com>
2022-03-02 16:54:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+#  React Router Dom
+
+## Pre Flight CheckList
+- Install styled-components
+```npm
+   npm install react-router-dom@6
+```
+- Linking <a>
+- Make sure all your Pages(components) are completed. Skeleton Framework.
+- LoginPage, DashBoardPage, PageNotFound .....
+
+
+### 1. Setup The Index.js With Browser Router
+- React router the <BrowserRouter/> component must wrap the App component. So this allows you to use Routing/Linking in you application.
+
+
+### 2. Setting Up Routes In The App Component
+- Importing page components.
+- Routes,
+- Route,
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
---
+++ src/ui/buttons/index.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: ${props => props.bgcolor || "transparent"};;
+  border: none;
+  border-radius: ${props => props.radius || "3px"};;
+  box-shadow: ${props => props.shadow || "0 0 3px 1px rgb(220, 220, 220)"};
+  margin: ${props => props.margin || "0"};;
+  padding: ${props => props.padding  || "0.5rem 2rem"};;
+  color: ${props => props.color || "grey"};
+  font-size: ${props => props.size  || "1rem"};;
+ 
+`;
+ 
+const IconButton = styled.button`   
+        border:none;
+        background-color: transparent;
+`;
+
+const SubmitButton = styled(Button)`
+    background-color: ${props => props.bgcolor || '#2563eb'};
+    color: ${props => props.color || '#f8fafc'}; 
+    font-weight: ${props => props.fw || '700'}; 
+    font-size: ${props => props.fs || '14px'};
+    width: ${props => props.width || "100%"};
+`
+
+export { Button, IconButton, SubmitButton };
